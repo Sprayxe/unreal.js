9ebc4a75af2218381482914d87bc762b7f066e16
Sprayxe <marcel.cieminski@web.de>
2021-03-27 11:46:19 +0100

    pain.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { FileProvider } from "./fileprovider/FileProvider";
 import { FGuid } from "./ue4/objects/core/misc/Guid";
 
 (async () => {
-    console.log("Create provider")
     const provider = new FileProvider("C:/Program Files/Epic Games/Fortnite/FortniteGame/Content/Paks")
     await provider.initialize()
     await provider.submitKey(FGuid.mainGuid, "0xD79DD62584A7401841F80C070F8B8B405A100F51511E918ABF4EF2BB981BCA0A")
